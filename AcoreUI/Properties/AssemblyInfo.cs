dba895353a1d30e7b8ebbc84745c840dec05577c
James Bass <jamesbass16180@gmail.com>
2024-06-07 16:38:18 -0500

    Add project files.

---
+++ AcoreUI/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AcoreUI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Coretelligent")]
+[assembly: AssemblyProduct("AcoreUI")]
+[assembly: AssemblyCopyright("Copyright © Coretelligent 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("49ea057a-fc3b-4a6b-b4c1-9c5797540ed3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AcoreUI/UnitTest1.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Security.Cryptography;
+using System.Threading;
+
+namespace AcoreUI
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestSearchStreetFighterVThenVerifyStreetFighterVIsDisplayed()
+        {
+            int waitingTime = 2000;
+            // Testing by name in inspect element
+            By googleSearchBar = By.Name("q");
+
+            // Google search button name
+            By googleSearchButton = By.Name("btnK");
+            By googleResultText = By.XPath(".//a//h3[text()='ACORE Capital: Welcome']"); // h3 inside a
+           // By googleIAgreeButton = By.Id("L2AGLb");
+
+            IWebDriver webDriver = new ChromeDriver();
+
+            Thread.Sleep(waitingTime);
+
+            webDriver.Navigate().GoToUrl("https://www.google.com");
+
+            Thread.Sleep(waitingTime);
+
+            //webDriver.FindElement(googleIAgreeButton).Click();
+
+            webDriver.Manage().Window.Maximize();
+
+            Thread.Sleep(waitingTime);
+
+            webDriver.FindElement(googleSearchBar).SendKeys("ACORE Capital");
+
+            Thread.Sleep(waitingTime);
+
+            webDriver.FindElement(googleSearchButton).Click();
+
+            Thread.Sleep(waitingTime);
+
+            var actualResultText = webDriver.FindElement(googleResultText);
+
+            Assert.IsTrue(actualResultText.Text.Equals("ACORE Capital: Welcome"));
+
+            webDriver.Quit();
+        }
+    }
+}
